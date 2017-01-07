d32d3b676cf442581ad867fabba5be8b1646be32
Jun Oh <jsohpennstater@gmail.com>
2017-01-07 16:02:26 -0500

    created playerprofile component

---
+++ react/src/components/PlayerProfile.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const PlayerProfile = props => {
+
+  return (
+    <div>
+      <p> {props.playerinfo.first_name} {props.playerinfo.last_name} {props.playerinfo.number} </p>
+      <p> Team: {props.playerinfo.team}, Position: {props.playerinfo.position} </p>
+      <p> College: {props.playerinfo.college} </p>
+      <p> Draft: {props.playerinfo.draft} </p>
+      <p> Height: {props.playerinfo.height}, Weight: {props.playerinfo.weight} </p>
+      <p> Birthday: {props.playerinfo.born}, Birthplace: {props.playerinfo.birthplace} </p>
+    </div>
+  )
+
+}
+
+export default PlayerProfile;
