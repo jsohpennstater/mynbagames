34fcb29d1a0037bfae2e3288ee74f71d4e90af6e
Jun Oh <jsohpennstater@gmail.com>
2017-01-15 20:40:20 -0500

    fixed names starting with following characters

--- app/controllers/api/v1/names_controller.rb
+++ app/controllers/api/v1/names_controller.rb
@@ -9,7 +9,7 @@ class Api::V1::NamesController < ApiController
         elsif Player.find_by(first_name: names[1], last_name: names[0])
           @names << Player.find_by(first_name: names[1], last_name: names[0])
         else
-          @names << Player.where("first_name ilike ? or last_name ilike ?", "%#{name}%", "%#{name}%" )
+          @names << Player.where("first_name ilike ? or last_name ilike ?", "#{name}%", "#{name}%" )
         end
       end
       @names.flatten!
