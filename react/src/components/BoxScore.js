58c7ee7702d0d500a2cfe8d7d0fd3e4072b25ea9
Jun Oh <jsohpennstater@gmail.com>
2017-01-18 16:23:50 -0500

    fixed display for gamelog nan for -

--- react/src/components/BoxScore.js
+++ react/src/components/BoxScore.js
@@ -1,16 +1,37 @@
 import React, { Component } from 'react'
 
 const BoxScore = props => {
+  let fgp = "";
+  let ftp = "";
+  let threep = "";
+
+  if (props.boxscore.fgm === "0" && props.boxscore.fga === "0") {
+    fgp = (props.boxscore.fgm / props.boxscore.fga).toFixed(3)
+  } else {
+    fgp = "-";
+  }
+
+  if (props.boxscore.ftm === "0" && props.boxscore.fta === "0") {
+    ftp = (props.boxscore.ftm / props.boxscore.fta).toFixed(3)
+  } else {
+    ftp = "-";
+  }
+
+  if (props.boxscore.threepm === "0" && props.boxscore.threepa === "0") {
+    threep = (props.boxscore.fgm / props.boxscore.fga).toFixed(3)
+  } else {
+    threep = "-";
+  }
 
   return (
     <tr>
       <td> {props.boxscore.min} </td>
       <td> {props.boxscore.fgm} - {props.boxscore.fga} </td>
-      <td> {(props.boxscore.fgm / props.boxscore.fga).toFixed(3)} </td>
+      <td> {fgp} </td>
       <td> {props.boxscore.threepm} - {props.boxscore.threepa} </td>
-      <td> {(props.boxscore.threepm / props.boxscore.threepa).toFixed(3)} </td>
+      <td> {ftp} </td>
       <td> {props.boxscore.ftm} - {props.boxscore.fta} </td>
-      <td> {(props.boxscore.ftm / props.boxscore.fta).toFixed(3)} </td>
+      <td> {threep} </td>
       <td> {props.boxscore.total_rebound} </td>
       <td> {props.boxscore.assist} </td>
       <td> {props.boxscore.block} </td>
