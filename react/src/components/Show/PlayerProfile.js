dbc5a86f562ab593d5ada95bc187c0c57259caeb
Jun Oh <jsohpennstater@gmail.com>
2017-01-18 21:34:01 -0500

    player name to h3 in playerprofile info

--- react/src/components/Show/PlayerProfile.js
+++ react/src/components/Show/PlayerProfile.js
@@ -4,7 +4,7 @@ const PlayerProfile = props => {
   return (
     <div className="profile row">
       <div className="small-12 medium-8 end columns">
-        <p> {props.profile.first_name} {props.profile.last_name} </p>
+        <h3> {props.profile.first_name} {props.profile.last_name} </h3>
         <p> {props.profile.number} {props.profile.position} | {props.profile.height}, {props.profile.weight} lbs | {props.profile.team} </p>
         <p> Born: {props.profile.born} in {props.profile.birthplace}</p>
         <p> Drafted: {props.profile.draft} </p>
