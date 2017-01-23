da7ea832ec0c423cb1e99c630301707e3d015851
Jun Oh <jsohpennstater@gmail.com>
2017-01-23 12:15:44 -0500

    reconfigured seed file for 10-12

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,8 +5,8 @@ require 'rubygems'
 require 'json'
 
 # STAT_INFO
-index = 1
-while index < 2
+index = 10
+while index < 13
   Dir.foreach("./public/boxscore/#{index}") do |file|
     next if file == "." or file == ".."
       game_data = File.read("./public/boxscore/#{index}/" + file)
@@ -52,47 +52,47 @@ while index < 2
   end
   index += 1
 end
-
-# PLAYER_INFO
-# index = 12
-# while index < 13
-  # Dir.foreach("./public/boxscore/#{index}") do |file|
-  #   next if file == "." or file == ".."
-  #   game_data = File.read("./public/boxscore/#{index}/" + file)
-  #   gamecode = file.chomp('.json')
-  #   sorted_game_data = JSON.parse(game_data)
-  #   sorted_game_data.each do |game|
-  #     if game["player_link"] != nil
-  #       doc = Nokogiri::HTML(open("http://scores.nbcsports.com#{game["player_link"]}"))
-  #       c = doc.css('tr')
-  #       playername = c[2].children[1].children[2].text
-  #       namesplit = playername.split(" ")
-  #       unless Player.find_by(last_name: namesplit[1], first_name: namesplit[0] )
-  #         saveplayer = Player.new
-  #         saveplayer.first_name = namesplit[0]
-  #         saveplayer.last_name = namesplit[1]
-  #         saveplayer.image = c[1].children[1].children[0].attributes["src"].value
-  #         saveplayer.number = c[2].children[1].children[0].text
-  #         saveplayer.team = c[5].children.children[1].children.text
-  #         saveplayer.height = c[5].children.children[3].text
-  #         saveplayer.born = c[5].children.children[5].text.strip
-  #         saveplayer.position = c[6].children[1].children.last.text
-  #         saveplayer.weight = c[6].children[2].children.last.text
-  #         saveplayer.birthplace = c[6].children[4].children.last.text
-  #         saveplayer.college = c[7].children[1].children.last.text
-  #         saveplayer.draft = c[7].children[2].children.last.text
-  #         saveplayer.save
-  #         puts "#{file}"
-  #       end
-  #     end
-  #   end
-  # end
+#
+# # PLAYER_INFO
+# index = 1
+# while index < 2
+#   Dir.foreach("./public/boxscore/#{index}") do |file|
+#     next if file == "." or file == ".."
+#     game_data = File.read("./public/boxscore/#{index}/" + file)
+#     gamecode = file.chomp('.json')
+#     sorted_game_data = JSON.parse(game_data)
+#     sorted_game_data.each do |game|
+#       if game["player_link"] != nil
+#         doc = Nokogiri::HTML(open("http://scores.nbcsports.com#{game["player_link"]}"))
+#         c = doc.css('tr')
+#         playername = c[2].children[1].children[2].text
+#         namesplit = playername.split(" ")
+#         unless Player.find_by(last_name: namesplit[1], first_name: namesplit[0] )
+#           saveplayer = Player.new
+#           saveplayer.first_name = namesplit[0]
+#           saveplayer.last_name = namesplit[1]
+#           saveplayer.image = c[1].children[1].children[0].attributes["src"].value
+#           saveplayer.number = c[2].children[1].children[0].text
+#           saveplayer.team = c[5].children.children[1].children.text
+#           saveplayer.height = c[5].children.children[3].text
+#           saveplayer.born = c[5].children.children[5].text.strip
+#           saveplayer.position = c[6].children[1].children.last.text
+#           saveplayer.weight = c[6].children[2].children.last.text
+#           saveplayer.birthplace = c[6].children[4].children.last.text
+#           saveplayer.college = c[7].children[1].children.last.text
+#           saveplayer.draft = c[7].children[2].children.last.text
+#           saveplayer.save
+#           puts "#{file}"
+#         end
+#       end
+#     end
+#   end
 #   index += 1
 # end
 
 # GAME_INFO
 # index = 1
-# while index < 2
+# while index < 13
 #   Dir.foreach("./public/games/#{index}") do |file|
 #     next if file == "." or file == ".."
 #       game_info = File.read("./public/games/#{index}/" + file)
