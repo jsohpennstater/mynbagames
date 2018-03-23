e288237e85f9bad9e6e72e8e44127f59884c053b
Jun <junoh421@gmail.com>
2018-03-23 12:16:49 -0400

    for now add rack-cors to app

--- config/application.rb
+++ config/application.rb
@@ -1,5 +1,4 @@
 require_relative 'boot'
-
 require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
@@ -8,8 +7,11 @@ Bundler.require(*Rails.groups)
 
 module Mynbagames
   class Application < Rails::Application
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource '*', :headers => :any, :methods => [:get]
+      end
+    end
   end
 end
